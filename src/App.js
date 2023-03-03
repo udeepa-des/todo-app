8071f95559cb8eaf332ac8022435fcd460a1355f
udeepa <it19002484@my.sliit.lk>
2023-03-03 17:36:26 +0530

    interactivity: events and tasks

--- src/App.js
+++ src/App.js
@@ -1,14 +1,43 @@
 import Todo from "./components/Todo";
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
+import React, {useState} from "react";
+import { nanoid } from "nanoid";
 
 function App(props) {
-  const taskList = props.tasks?.map((task) => (
+  const [tasks, setTasks] = useState(props.tasks);
+
+  function addTask(name) {
+    const newTask = { id: `todo-${nanoid()}`, name, completed: false };
+    setTasks([...tasks, newTask])
+  }
+
+  function toggleTaskCompleted(id) {
+    const updatedTasks = tasks.map((task) => {
+      //check if the task has the same ID as the edited task
+      if(id === task.id) {
+        //object spread to make a new object
+        //'completed' prop has been inverted
+        return {...task, completed: !task.completed}
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
+  }
+
+  function deleteTask(id) {
+    const remainingTasks = tasks.filter((task) => id !== task.id);
+    setTasks(remainingTasks);
+  }
+
+  const taskList = tasks.map((task) => (
       <Todo
         id={task.id}
         name={task.name}
         completed={task.completed}
         key={task.id}
+        toggleTaskCompleted={toggleTaskCompleted}
+        deleteTask = {deleteTask}
       />
     )
   );
@@ -19,14 +48,16 @@ function App(props) {
       />
     )
   );
+  const tasksNoun = taskList.length !== 1 ? 'tasks' : 'task';
+  const headingText = `${taskList.length} ${tasksNoun} remaining`;
   return (
     <div className="todoapp stack-large">
       <h1>Todo</h1>
-      <Form />
+      <Form  addTask={addTask}/>
       <div className="filters btn-group stack-exception">
         {filterList}
       </div>
-      <h2 id="list-heading">3 tasks remaining</h2>
+      <h2 id="list-heading">{headingText}</h2>
       <ul
         role="list"
         className="todo-list stack-large stack-exception"
