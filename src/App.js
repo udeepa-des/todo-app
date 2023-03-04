aeb52d337f2fa7ec445d0cb6de88f8d7c4943a86
udeepa <it19002484@my.sliit.lk>
2023-03-04 13:49:57 +0530

    Accessibility highlight

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import Todo from "./components/Todo";
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
-import React, {useState} from "react";
+import React, {  useState, useRef, useEffect } from "react";
 import { nanoid } from "nanoid";
+
+//focus on the heading
+function usePrevious(value) {
+  const ref = useRef();
+  useEffect(() => {
+    ref.current = value;
+  });
+  return ref.current;
+}
 
 const FILTER_MAP = {
   All: () => true,
@@ -74,6 +83,17 @@ function App(props) {
 
   const tasksNoun = taskList.length !== 1 ? 'tasks' : 'task';
   const headingText = `${taskList.length} ${tasksNoun} remaining`;
+
+  //focus on the heading
+  const listHeadingRef = useRef(null);
+  const prevTaskLength = usePrevious(tasks.length);
+
+  useEffect(() => {
+    if(tasks.length - prevTaskLength === -1) {
+      listHeadingRef.current.focus();
+    }
+  }, [tasks.length, prevTaskLength]);
+
   return (
     <div className="todoapp stack-large">
       <h1>Todo</h1>
@@ -81,7 +101,7 @@ function App(props) {
       <div className="filters btn-group stack-exception">
         {filterList}
       </div>
-      <h2 id="list-heading">{headingText}</h2>
+      <h2 id="list-heading" tabIndex="-1" ref={listHeadingRef}>{headingText}</h2>
       <ul
         role="list"
         className="todo-list stack-large stack-exception"
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,8 +1,20 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
+
+function usePrevious(value) {
+  const ref = useRef();
+  useEffect(() => {
+    ref.current = value;
+  });
+  return ref.current;
+}
 
 export default function Todo(props) {
     const [isEditing, setEditing] = useState(false);
     const [newName, setNewName] = useState('');
+    const wasEditing = usePrevious(isEditing);
+
+    const editFieldRef = useRef(null);
+    const editButtonRef = useRef(null);
 
     function handleChange(e) {
         setNewName(e.target.value);
@@ -27,6 +39,7 @@ export default function Todo(props) {
               type="text" 
               value={newName}
               onChange={handleChange}
+              ref={editFieldRef}
             />
           </div>
           <div className="btn-group">
@@ -55,7 +68,12 @@ export default function Todo(props) {
               </label>
             </div>
             <div className="btn-group">
-              <button type="button" className="btn" onClick={() => setEditing(true)}>
+              <button 
+                type="button" 
+                className="btn" 
+                onClick={() => setEditing(true)}
+                ref={editButtonRef}
+              >
                 Edit <span className="visually-hidden">{props.name}</span>
               </button>
               <button
@@ -69,5 +87,14 @@ export default function Todo(props) {
         </div>
       );
 
+      useEffect(() => {
+        if(!wasEditing && isEditing) {
+          editFieldRef.current.focus();
+        }
+        if(wasEditing && !isEditing) {
+          editButtonRef.current.focus();
+        }        
+      }, [wasEditing, isEditing]);
+      
     return <li className="todo">{isEditing ? editingTemplate : viewTemplate}</li>; 
   }
