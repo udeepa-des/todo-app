383078519b335bcc70005fabe74f1349b03cbba9
udeepa <it19002484@my.sliit.lk>
2023-03-04 12:52:56 +0530

    editing, filtering and conditional rendering

--- src/App.js
+++ src/App.js
@@ -4,8 +4,17 @@ import FilterButton from "./components/FilterButton";
 import React, {useState} from "react";
 import { nanoid } from "nanoid";
 
+const FILTER_MAP = {
+  All: () => true,
+  Active: (task) => !task.completed,
+  Completed: (task) => task.completed
+};
+
+const FILTER_NAMES = Object.keys(FILTER_MAP);
+
 function App(props) {
   const [tasks, setTasks] = useState(props.tasks);
+  const [filter, setFilter] = useState('All');
 
   function addTask(name) {
     const newTask = { id: `todo-${nanoid()}`, name, completed: false };
@@ -30,7 +39,17 @@ function App(props) {
     setTasks(remainingTasks);
   }
 
-  const taskList = tasks.map((task) => (
+  function editTask(id, newName) {
+    const editedTaskList = tasks.map((task) => {
+      if(id === task.id) {
+        return{...task, name: newName}
+      }
+      return task;
+    });
+    setTasks(editedTaskList);
+  }
+
+  const taskList = tasks.filter(FILTER_MAP[filter]).map((task) => (
       <Todo
         id={task.id}
         name={task.name}
@@ -38,16 +57,21 @@ function App(props) {
         key={task.id}
         toggleTaskCompleted={toggleTaskCompleted}
         deleteTask = {deleteTask}
+        editTask = {editTask}
       />
     )
   );
-  const filterList = props.filters?.map((filter) => (
-      <FilterButton
-        pressed={filter.pressed}
-        name={filter.name}
+
+  const filterList = FILTER_NAMES.map((name) => (
+      <FilterButton 
+        key={name} 
+        name={name}
+        isPressed={name === filter}
+        setFilter={setFilter} 
       />
     )
   );
+
   const tasksNoun = taskList.length !== 1 ? 'tasks' : 'task';
   const headingText = `${taskList.length} ${tasksNoun} remaining`;
   return (
